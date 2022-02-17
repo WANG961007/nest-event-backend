5b372185f887fd33dccb84eb4a482deb30f734f3
WANG961007 <mw1023@connect.hku.hk>
2022-02-17 14:46:23 +0800

    [xuejing,Wang]bug occured: store the correct code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {EventsController} from "./events/events.controller";
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {Event} from "./events/event.entity";
 import { EventsModule } from './events/events.module';
 import {AppJapanService} from "./app.japan.service";
 import {AppDummy} from "./app.dummy";
