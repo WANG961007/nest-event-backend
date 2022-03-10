3d6745ac544f7ce26ac5c2c3348f6b717aba22dc
WANG961007 <mw1023@connect.hku.hk>
2022-03-10 13:57:04 +0800

    [xuejing,Wang]refactor: modify the method of project link to the mysql

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -1,16 +1,16 @@
 import {TypeOrmModuleOptions} from "@nestjs/typeorm";
 import {registerAs} from "@nestjs/config";
 
-export default registerAs('orm.config',(): TypeOrmModuleOptions => ({
+export default registerAs('orm.config', (): TypeOrmModuleOptions => ({
     type: 'mysql',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        entities: ["dist/**/*.entity{.ts,.js}"],
-       // entities: [Event,Attendee],
+    host: "mysql",
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    // entities: [Event,Attendee],
 
-        synchronize: true,
-        logging: true
+    synchronize: true,
+    logging: true
 }));
